a76fafb1b910d1595954c4db4078a7e23ff7dcca
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-03-05 16:00:16 -0300

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,1 +1,16 @@
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] wordsOfPangram = pangram.Split(" ");
+string pangramReversed = "";
 
+foreach (string word in wordsOfPangram)
+{
+  char[] letters = word.ToCharArray();
+
+  Array.Reverse(letters);
+
+  string reversedWord = String.Join("", letters);
+
+  pangramReversed += reversedWord + " ";
+
+}
+System.Console.WriteLine(pangramReversed);
